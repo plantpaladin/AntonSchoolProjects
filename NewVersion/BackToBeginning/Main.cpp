19df20b3d78fd74ff17846524b365bb1802c689a
plantpaladin <plantpaladin@gmail.com>
2016-12-01 14:13:25 +0100

    Update Main.cpp

--- NewVersion/BackToBeginning/Main.cpp
+++ NewVersion/BackToBeginning/Main.cpp
@@ -1,20 +1,40 @@
 #include <SFML/Window.hpp>
-
+#include "MainLoop.h"
+#include <Windows.h>
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My window");
+	sf::RenderWindow * window = new sf::RenderWindow(sf::VideoMode(640, 960), "Game On");
+	
+	MainLoop * gameLoop = new MainLoop;
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
+
+	sf::Clock deltaClock;
+	sf::Time deltaTime;
 
 	// run the program as long as the window is open
-	while (window.isOpen())
+	while (window->isOpen())
 	{
 		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (window->pollEvent(event))
 		{
 			// "close requested" event: we close the window
 			if (event.type == sf::Event::Closed)
-				window.close();
+				window->close();
+			
+
 		}
+		
+		gameLoop->update(deltaTime.asSeconds());
+		if (gameLoop->collide()==true)//returns true if the player dies
+		{
+			return 0;
+		}
+			window->clear(sf::Color::Blue);
+			gameLoop->render(window);
+			window->display();
+			deltaTime = deltaClock.restart();
 	}
 
 	return 0;
