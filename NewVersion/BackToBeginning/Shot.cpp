bc7da8efc2d0a768f1f4db3f78b7366b8cd8bcc8
plantpaladin <plantpaladin@gmail.com>
2016-12-01 14:31:10 +0100

    Update Shot.cpp

--- NewVersion/BackToBeginning/Shot.cpp
+++ NewVersion/BackToBeginning/Shot.cpp
@@ -2,11 +2,27 @@
 
 
 
-Shot::Shot()
+Shot::Shot(float x, float y, float radius, float speed, sf::Sprite * sprite)
 {
+	m_x = x;
+	m_y = y;
+	m_radius = radius;
+	m_speed = speed;
+	m_sprite = sprite;
 }
 
+void Shot::newPosition(sf::Vector2f newPosition)
+{
+	m_x = newPosition.x;
+	m_y = newPosition.y;
+}
+
+void Shot::update(float deltaTime)
+{
+	m_y += m_speed*deltaTime;
+}
 
 Shot::~Shot()
 {
+	delete m_sprite;
 }
