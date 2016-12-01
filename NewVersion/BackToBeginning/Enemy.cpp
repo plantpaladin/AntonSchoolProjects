d09bbc9eb9a54eeadc3635f48fe18ef76368e187
plantpaladin <plantpaladin@gmail.com>
2016-12-01 14:22:07 +0100

    Update Enemy.cpp

--- NewVersion/BackToBeginning/Enemy.cpp
+++ NewVersion/BackToBeginning/Enemy.cpp
@@ -1,22 +1,70 @@
 #include "Enemy.h"
 #include <string>
 #include <iostream>
+static float enemyFiringSpeed = 2.0;
+static float maxX = 512;//the width of the screen minus the radius of the ship * 2
 
-
-Enemy::Enemy(int x,int y, int radius,std::string spriteName)
+Enemy::Enemy(float x,float y, float radius,float speed,sf::Sprite * sprite)
 {
 	m_x = x;
 	m_y = y;
 	m_radius = radius;
-	if (!m_texture.loadFromFile(spriteName, sf::IntRect(10, 10, 64, 64)))
+	m_sprite = sprite;
+	m_speed = speed;
+	movingRight = 1;
+	m_timeSinceLastShot = 0;
+}
+
+void Enemy::newPosition(int x)
+{
+	movingRight = 1;
+	m_y = 0;
+	m_x = x;
+}
+
+void Enemy::update(float deltaTime)//delta time in seconds
+{
+	m_timeSinceLastShot += deltaTime;
+	
+	m_y += m_speed*deltaTime;
+	float nextX = m_x + deltaTime*m_speed*movingRight;
+	
+	if (movingRight ==true)
 	{
-		std::cout << "Failure \n";
+		if (nextX > maxX)
+		{
+			m_x = maxX;
+			movingRight = -1;
+		}
+		else
+		{
+			m_x = nextX;
+		}
+	}
+	else
+	{
+		if (nextX < m_radius)
+		{
+			m_x = m_radius;
+			movingRight = 1;
+		}
+		else
+		{
+			m_x = nextX;
+		}
 	}
 }
 
-void Enemy::update()
+bool Enemy::canShoot()
 {
+	if (m_timeSinceLastShot >= enemyFiringSpeed)
+	{
+		m_timeSinceLastShot -= enemyFiringSpeed;
+		return true;
+	}
+	return false;
 }
 Enemy::~Enemy()
 {
+	delete m_sprite;
 }
